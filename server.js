758c3c9935c2687bed1c19e077300755ab1e66ed
ezb_ryankenidy <kenidyryan@gmail.com>
2023-10-29 11:57:02 +0700

    config logger using winston

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require("./app");
 const port = process.env.SERVER_PORT | 3000;
+const logger = require("./src/util/logger");
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
+  logger.log({
+    level: "info",
+    message: "Testing logging",
+  });
 });
