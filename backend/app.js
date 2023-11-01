cd2bd87a48df36511eb86713975a93398da352a8
ezb_ryankenidy <kenidyryan@gmail.com>
2023-11-01 16:09:54 +0700

    remove test branch

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,6 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-console.log(2);
-
 app.get("/", (req, res) => res.json({ message: "Docker is hard !!, test2" }));
-
-console.log("test");
 
 module.exports = app;
